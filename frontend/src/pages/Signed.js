29cdb0f73fd93f91308926cfc51f6bdb2ba6b3f5
nont18 <nont.a@ku.th>
2025-11-19 23:53:03 +0700

    commit

--- frontend/src/pages/Signed.js
+++ frontend/src/pages/Signed.js
@@ -9,7 +9,7 @@ function Signed() {
     const message = e.target.message.value;
 
     try {
-      const res = await fetch('/api/send', {
+      const res = await fetch('/api/send.js', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ name, services, message }),
