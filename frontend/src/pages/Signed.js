40e8d5a9d20640ca30083619e217803bc2f50deb
nont18 <nont.a@ku.th>
2025-11-22 17:28:32 +0700

    creds

--- frontend/src/pages/Signed.js
+++ frontend/src/pages/Signed.js
@@ -7,7 +7,7 @@ function Signed() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const res = await axios.post(`https://mywebsite-c4634ejog-nont18s-projects.vercel.app/api/send`, form)
+    const res = await axios.post(`https://mywebsite-hr43uke0n-nont18s-projects.vercel.app/api/send`, form)
     // const res = await axios.post("http://localhost:8000/api/send", form)
 
     if(res.data.success){
