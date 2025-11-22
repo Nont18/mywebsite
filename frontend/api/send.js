4b7866153f45f9046cb93105dad1f5a5245114c1
nont18 <nont.a@ku.th>
2025-11-22 17:05:07 +0700

    creds

--- frontend/api/send.js
+++ frontend/api/send.js
@@ -133,7 +133,7 @@ app.post("/api/send", async (req, res) => {
     const auth = new google.auth.GoogleAuth({
     // keyFile: "../mywebsite/frontend/api/credentials.json",
     // keyFile: "./api/google-api-credentials.json",
-    credentials:JSON.parse(credentials),
+    credentials:credentials,
     scopes: ["https://www.googleapis.com/auth/spreadsheets"]
   });
 
