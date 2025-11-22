24716300661b60ce4b195a9c8f413640ca1b039a
nont18 <nont.a@ku.th>
2025-11-22 12:21:32 +0700

    fix creds

--- frontend/api/send.js
+++ frontend/api/send.js
@@ -130,7 +130,20 @@ app.post("/api/send", async (req, res) => {
     
     const auth = new google.auth.GoogleAuth({
     // keyFile: "../mywebsite/frontend/api/credentials.json",
-    keyFile: "./api/google-api-credentials.json",
+    // keyFile: "./api/google-api-credentials.json",
+    keyFile: {
+      "type": "service_account",
+      "project_id": "sheetformywebsite",
+      "private_key_id": process.env.GOOGLE_PRIVATE_KEY_ID,
+      "private_key": process.env.GOOGLE_PRIVATE_KEY,
+      "client_email": process.env.GOOGLE_CLIENT_EMAIL,
+      "client_id": process.env.GOOGLE_CLIENT_ID,
+      "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+      "token_uri": "https://oauth2.googleapis.com/token",
+      "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+      "client_x509_cert_url": process.env.GOOGLE_CLIENT_X509_CERT_URL,
+      "universe_domain": "googleapis.com"
+    },
     scopes: ["https://www.googleapis.com/auth/spreadsheets"]
   });
 
